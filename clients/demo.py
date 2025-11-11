240ed7d1f56f7bcb2aa2acc96760d4286bb642d1
JP Borgneth <jp.borgneth@gmail.com>
2025-11-11 19:18:36 -0300

    updated client demo

--- clients/demo.py
+++ clients/demo.py
@@ -23,18 +23,22 @@ def main():
     u1 = user.CreateUser(user_pb2.CreateUserRequest(name="Alice", email="alice@example.com"))
     u2 = user.CreateUser(user_pb2.CreateUserRequest(name="Bob", email="bob@example.com"))
     print("Users created:", [u1, u2])
+    print("---------------------------")
 
-    order.CreateOrder(order_pb2.CreateOrderRequest(user_id=u1.id, items=["book", "pen"], total=50.0))
-    order.CreateOrder(order_pb2.CreateOrderRequest(user_id=u1.id, items=["notebook"], total=25.0))
-    order.CreateOrder(order_pb2.CreateOrderRequest(user_id=u2.id, items=["mouse"], total=80.0))
+    o1 = order.CreateOrder(order_pb2.CreateOrderRequest(user_id=u1.id, items=["book", "pen"], total=50.0))
+    o2 = order.CreateOrder(order_pb2.CreateOrderRequest(user_id=u1.id, items=["notebook"], total=25.0))
+    o3 = order.CreateOrder(order_pb2.CreateOrderRequest(user_id=u2.id, items=["mouse"], total=80.0))
+    print("Orders created:")
+    print(o1)
+    print(o2)
+    print(o3)
+    print("---------------------------")
 
     report_alice = report.GetUserOrdersReport(report_pb2.UserOrdersReportRequest(user_id=u1.id))
     print("Report Alice:", report_alice)
 
-    top = report.GetTopUsersByOrders(report_pb2.TopUsersByOrdersRequest(top_n=5))
-    for e in top.entries:
-        print(f"Top: {e.user_name} | count={e.orders_count} total={e.orders_total}")
-
+    report_bob = report.GetUserOrdersReport(report_pb2.UserOrdersReportRequest(user_id=u2.id))
+    print("Report Bob:", report_bob)
 
 if __name__ == "__main__":
     main()
